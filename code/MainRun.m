41659a28772ac5d0488fca3de55b7be069da9293
Abderrahmen0102 <94995612+Abderrahmen0102@users.noreply.github.com>
2022-12-26 19:24:59 +0300

    Update MainRun.m

--- code/MainRun.m
+++ code/MainRun.m
@@ -33,19 +33,6 @@ ub = [500,500,500,500,500,500,1,1,1,1,1,1,500,500,500];
 XStart = [5,0,5,0,5,5,1,0,1,0,1,1,5,10,5];
 
 X0=XStart;
-%------------------------NLP Fmincon Solvers------------------------
-%fmincon use the SQP algorithm, also with the parallel computing
-%option1 = optimoptions('fmincon','UseParallel',true,'Display','iter','Algorithm','sqp','TolFun', 1e-10, 'TolX', 1e-10, 'TolCon', 1e-10, 'MaxFunEvals', 2000);
-%option2 = optimoptions('fmincon','UseParallel',true,'Display','iter','Algorithm','interior-point','TolFun', 1e-10, 'TolX', 1e-10, 'TolCon', 1e-10, 'MaxFunEvals', 4000);
-
-%[x1,fval1] = fmincon(@(x)ObjfunSplit(x,k,h,b,N,Lambda,Lr,Lw,Or,Ow,Pw,w),XStart,A,bq,Aeq,beq,lb,ub,@(x)ConNonlA(x,BigM,N,Lambda,Lw),option1);
-
-%X0 = roundn(x1,-1);
-%for j=k+1:2*k;
-    %X0(j) = ceil(X0(j));
-%end
-%X0 = round(X0);
-%y1= fval1;
 
 %----------------------Knitro Solver----------------------
 xType = [1,1,1,1,1,1,2,2,2,2,2,2,1,1,1];
@@ -86,28 +73,5 @@ Knitrooptions = optimset('Algorithm', 'active-set', 'Display','iter','MaxIter',1
 % Integer Constraints
 %xtype = 'IIIIIIBBBBBBIII';
 
-%---------------IPOPT-------------
-% Options
-%opts = optiset('solver','IPOPT','display','iter');
-% Build OPTI Problem
-%Opt = opti('fun',@(x)ObjfunSplit(x,k,h,b,N,Lambda,Lr,Lw,Or,Ow,Pw),'nlmix',@(x)ConNonOPTI(x,SmallM,BigM),nlrhs,nle,'bounds',lb,ub,'x0',XStart,'options',opts);
 
-%----------------Nomad------------
-%X0=XStart;
-%opts = optiset('solver','Nomad','display','iter');
-% Build OPTI Problem
-%Opt = opti('fun',@(x)ObjfunSplit(x,k,h,b,N,Lambda,Lr,Lw,Or,Ow,Pw),'nlmix',@(x)ConNonOPTI(x,SmallM,BigM),nlrhs,nle,'bounds',lb,ub,'x0',X0,'xtype',xtype,'options',opts);
-
-% Solve NLP
-%[x,fval,exitflag,info] = solve(Opt,X0);
-
-
-
-%------------GA Options-----------------------------------------------
-%GA Options
-%IntCon  = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15];
-%nvars = 15;
-%gaoptions = gaoptimset('StallGenLimit',50,'TolFun',1e-10,'Generations',1000,'PlotFcns',@gaplotbestfun,'Display','iter','UseParallel',true);
-
-%[x,fval,exitflag] = ga(@(x)ObjfunSplit(x,k,h,b,N,Lambda,Lr,Lw,Or,Ow,Pw),nvars,A,[],Aeq,beq,lb,ub,@(x)ConNonl(x,SmallM,BigM),IntCon,gaoptions);
 
